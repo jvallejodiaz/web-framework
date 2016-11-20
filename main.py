74a1eebd58354dc90af48e35d24cf2b37a116952
jvallejodiaz <jvallejodiaz@gmail.com>
2016-11-20 18:50:40 +0100

    [ADD] Created App object for the framework.

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
+import os
 from werkzeug.wrappers import Request, Response
+from werkzeug.wsgi import SharedDataMiddleware
 
-_request_stack = werkzeug.local.LocalStack()
+class App(object):
 
-@Request.application
-def application(request):
-    return Response('Hello World!')
+    def dispatch_request(self, request):
+        return Response('Hello World!')
 
-def run(host, port, funct):
-    from werkzeug.serving import run_simple
-    run_simple(host, port, funct)
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+def create_app(redis_host='localhost', redis_port=6379, with_static=True):
+    app = App()
+    if with_static:
+        app.wsgi_app = SharedDataMiddleware(app.wsgi_app, {
+            '/static':  os.path.join(os.path.dirname(__file__), 'static')
+        })
+    return app
 
 if __name__ == '__main__':
-    run('localhost', 4000, application)
+    from werkzeug.serving import run_simple
+    app = create_app()
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
